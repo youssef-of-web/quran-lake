e947d48127b6a751e7675c5d840c68cf895a61af
Souhail Krissaane <61523279+SouhailKrs@users.noreply.github.com>
2024-05-29 03:17:52 +0100

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-			use: [{ loader: "@svgr/webpack", options: { icon: true } }],
-    });
-    return config;
-  }
 };
 
 export default withNextIntl(nextConfig);

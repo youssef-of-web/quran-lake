b459a237ccfff4a3ab6757f7c2271aa851f24fae
Mansouri youssef <mansouriyoussef1991@gmail.com>
2025-05-12 22:40:34 +0100

    Fix PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import withPWA from 'next-pwa';
+
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
@@ -6,4 +8,8 @@ const nextConfig = {
   reactStrictMode: false,
 };
 
-export default withNextIntl(nextConfig);
+export default withPWA({
+  dest: 'public', // destination directory for the PWA files
+  register: true, // register the PWA service worker
+  skipWaiting: true, // skip waiting for service worker activation
+})(withNextIntl(nextConfig));
